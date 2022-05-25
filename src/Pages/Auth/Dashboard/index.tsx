ce0102cc7c4edee666e6f6d5f656eac5ce2ea055
k3lm4n <69762912+k3lm4n@users.noreply.github.com>
2022-05-25 07:55:12 +0100

    Dashboard 30%

--- src/Pages/Auth/Dashboard/index.tsx
+++ src/Pages/Auth/Dashboard/index.tsx
@@ -1,32 +1,155 @@
 import React, {useCallback, useMemo, useRef} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import BottomSheet from '@gorhom/bottom-sheet';
-import {Container, ContainerInformation} from './style';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import {ProgressBar, Colors} from 'react-native-paper';
+import BottomSheet, {
+  BottomSheetView,
+  useBottomSheetDynamicSnapPoints,
+  useBottomSheetSpringConfigs,
+  useBottomSheetTimingConfigs,
+} from '@gorhom/bottom-sheet';
+import {PlusCircle} from 'phosphor-react-native';
+import {Easing} from 'react-native-reanimated';
+import {
+  Container,
+  ContainerInformation,
+  TopInf,
+  ButtonMore,
+  Title,
+  More,
+  RightView,
+  LeftView,
+  Box,
+  SectionTop,
+  ImgType,
+  TitleBox,
+  VolBox,
+  Bar,
+} from './style';
+import LinearGradient from 'react-native-linear-gradient';
 
 const DashBoard: React.FC = () => {
-  // ref
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const initialSnapPoints = useMemo(() => ['65%', 'CONTENT_HEIGHT'], []);
 
-  // variables
-  const snapPoints = useMemo(() => ['25%', '70%'], []);
+  const {
+    animatedHandleHeight,
+    animatedSnapPoints,
+    animatedContentHeight,
+    handleContentLayout,
+  } = useBottomSheetDynamicSnapPoints(initialSnapPoints);
 
-  // callbacks
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
+  const animationConfigs_1 = useBottomSheetTimingConfigs({
+    duration: 1000,
+    easing: Easing.exp,
+  });
+
+  const animationConfigs_2 = useBottomSheetSpringConfigs({
+    damping: 80,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.1,
+    restSpeedThreshold: 0.1,
+    stiffness: 500,
+  });
 
   return (
     <Container>
       <BottomSheet
         ref={bottomSheetRef}
-        index={1}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}>
-        <ContainerInformation>
-          <Text>Awesome 🎉</Text>
-        </ContainerInformation>
+        snapPoints={animatedSnapPoints}
+        handleHeight={animatedHandleHeight}
+        contentHeight={animatedContentHeight}
+        animationConfigs={animationConfigs_2}>
+        <BottomSheetView onLayout={handleContentLayout}>
+          <ContainerInformation>
+            <TopInf>
+              <LeftView>
+                <Title>Transaçōes</Title>
+              </LeftView>
+              <RightView>
+                <ButtonMore>
+                  <PlusCircle
+                    size={32}
+                    weight="fill"
+                    color="#262626"
+                    style={{marginTop: 8, marginLeft: 8}}
+                  />
+                  <More>Ver mais</More>
+                </ButtonMore>
+              </RightView>
+            </TopInf>
+            <SectionTop>
+              <LeftView>
+                <Box>
+                  <LinearGradient
+                    colors={[
+                      'rgba(207, 207, 207, 0.26)',
+                      'rgba(207, 207, 207, 0)',
+                    ]}
+                    style={{flex: 1, borderRadius: 10}}>
+                    <ImgType source={require('../../../assets/Saly-11.png')} />
+                    <TitleBox>Pagamentos</TitleBox>
+                    <VolBox>20.12Kz</VolBox>
+                    <Bar>
+                      <ProgressBar progress={0.5} color={'#BFBCE8'} />
+                    </Bar>
+                  </LinearGradient>
+                </Box>
+
+                <Box>
+                  <LinearGradient
+                    colors={[
+                      'rgba(207, 207, 207, 0.26)',
+                      'rgba(207, 207, 207, 0)',
+                    ]}
+                    style={{flex: 1, borderRadius: 10}}>
+                    <ImgType source={require('../../../assets/Saly-42.png')} />
+                    <TitleBox>Transferências</TitleBox>
+                    <VolBox>20.12Kz</VolBox>
+                    <Bar>
+                      <ProgressBar progress={0.5} color={'#BFBCE8'} />
+                    </Bar>
+                  </LinearGradient>
+                </Box>
+              </LeftView>
+
+              <RightView>
+                <Box>
+                  <LinearGradient
+                    colors={[
+                      'rgba(207, 207, 207, 0.26)',
+                      'rgba(207, 207, 207, 0)',
+                    ]}
+                    style={{flex: 1, borderRadius: 10}}>
+                    <ImgType source={require('../../../assets/Sally-4.png')} />
+                    <TitleBox>Depósitos</TitleBox>
+                    <VolBox>20.12Kz</VolBox>
+                    <Bar>
+                      <ProgressBar progress={0.5} color={'#BFBCE8'} />
+                    </Bar>
+                  </LinearGradient>
+                </Box>
+                <Box>
+                  <LinearGradient
+                    colors={[
+                      'rgba(207, 207, 207, 0.26)',
+                      'rgba(207, 207, 207, 0)',
+                    ]}
+                    style={{flex: 1, borderRadius: 10}}>
+                    <ImgType source={require('../../../assets/Saly-45.png')} />
+                    <TitleBox>Outros</TitleBox>
+                    <VolBox>20.12Kz</VolBox>
+                    <Bar>
+                      <ProgressBar progress={0.5} color={'#BFBCE8'} />
+                    </Bar>
+                  </LinearGradient>
+                </Box>
+              </RightView>
+            </SectionTop>
+          </ContainerInformation>
+        </BottomSheetView>
       </BottomSheet>
     </Container>
   );
 };
+
 export default DashBoard;
--- src/Pages/Auth/Dashboard/style.tsx
+++ src/Pages/Auth/Dashboard/style.tsx
@@ -1,33 +1,96 @@
 import styled from 'styled-components/native';
 
-
 export const Container = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #E5E5E5;
-    padding: 40px 24px;
-`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #e5e5e5;
+  padding: 40px 24px;
+`;
 
 export const ImageTop = styled.Image`
-    position: absolute;
-    top: 0;
-`
+  position: absolute;
+  top: 0;
+`;
 export const ImageBottom = styled.Image`
-    position: absolute;
-    bottom: 0;
-    left: 0;
-
-`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
 export const ContainerInformation = styled.View`
-    flex: 1;
-    width: 100%;
-    margin-top: 16px;
+  width: 95%;
+  margin-top: 20px;
+  margin-left: 3%;
 `;
 
 export const Title = styled.Text`
-    color: #1D5C63;
-    font-size:  20px;
-    font-weight: bold;
-    text-align: center;
-`
+  color: #1d5c63;
+  font-size: 24px;
+  font-weight: bold;
+  padding-top: 8px;
+`;
+
+export const TopInf = styled.View`
+  flex-direction: row;
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const ButtonMore = styled.TouchableOpacity`
+  border: 1px solid #e8e8e8;
+  border-radius: 222222px;
+  flex-direction: row;
+`;
+export const More = styled.Text`
+  color: #262626;
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 15px;
+  padding-left: 5px;
+`;
+
+export const LeftView = styled.View`
+  flex: 1;
+`;
+
+export const RightView = styled.View`
+  align-items: flex-end;
+  flex: 1;
+`;
+
+export const Box = styled.TouchableOpacity`
+  width: 97%;
+  height: 200px;
+
+  border-radius: 10px;
+  margin-top: 7%;
+`;
+
+export const SectionTop = styled.View`
+  flex-direction: row;
+`;
+export const ImgType = styled.Image`
+  width: 80px;
+  height: 80px;
+  margin-top: 20px;
+  margin-left: 25px;
+`;
+export const TitleBox = styled.Text`
+  color: #262626;
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: 25px;
+`;
+export const VolBox = styled.Text`
+  color: #7a7a7a;
+  font-size: 25px;
+  font-weight: lighter;
+  margin-left: 24px;
+`;
+export const Bar = styled.View`
+margin-left: 25px;
+margin-top: 5px;
+  width: 80%;
+`;
