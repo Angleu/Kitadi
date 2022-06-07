9f06856b01831b6a3008c2d0b0239d6c6c901c3f
Angleu <angleuzua99@gmail.com>
2022-06-07 15:37:38 +0100

    update deposit

--- src/components/AvailableAmount/index.tsx
+++ src/components/AvailableAmount/index.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import LinearGradient from "react-native-linear-gradient";
-import { Amount,TextAmount,TextSection,ViewRight,IconAlign } from "./style";
+import { Amount, TextAmount, TextSection, ViewRight, IconAlign } from "./style";
 import { Info } from "phosphor-react-native";
+import AuthenticationContext from "../../context/Authentication";
 
 
 
 export const AvailableAmount = () => {
-    return(<LinearGradient style={{ width: '100%', borderRadius: 10,marginBottom:35 }} start={{ x: 0, y: 1 }} end={{ x: 0, y: 0 }} colors={['rgba(45, 103, 109, 0.7)', 'rgba(29, 92, 99, 1)']}>
+  const globalContext = useContext(AuthenticationContext)
+  return (<LinearGradient style={{ width: '100%', borderRadius: 10, marginBottom: 35 }} start={{ x: 0, y: 1 }} end={{ x: 0, y: 0 }} colors={['rgba(45, 103, 109, 0.7)', 'rgba(29, 92, 99, 1)']}>
     <Amount>
 
       <TextSection>Saldo Disponivel</TextSection>
-      <TextAmount>40000 Kz</TextAmount>
+      <TextAmount>{globalContext.account.balance} Kz</TextAmount>
       <ViewRight>
         <IconAlign>
           <Info color="#fff" size={38} />
